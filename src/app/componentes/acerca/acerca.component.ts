b365f968af2187bb33742990d73722862626e29f
Fabian <you@example.com>
2022-05-25 02:51:34 -0300

    7mo commit

--- src/app/componentes/acerca/acerca.component.ts
+++ src/app/componentes/acerca/acerca.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PorfolioService } from 'src/app/servicios/porfolio.service';
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-acerca',
   templateUrl: './acerca.component.html',
   styleUrls: ['./acerca.component.css']
 })
 export class AcercaComponent implements OnInit {
-  acerca:any;
+  
+  @Input()acerca:any;
+  
   constructor(private datosPorfolio:PorfolioService) { }
 
   ngOnInit(): void {
@@ -14,6 +17,56 @@ export class AcercaComponent implements OnInit {
       console.log(data);
       this.acerca=data;
     });
+    
   }
-
 }
+
+
+function cambiar_parrafo():void{
+  document.getElementById("edit-acerca");
+  document.getElementById("filel");
+  let texto=document.getElementById("text-acerca");
+  console.log(texto);
+};
+cambiar_parrafo();
+
+
+function myFunction2(valor:any):void{
+       document.getElementById("text-acerca");
+  }
+  myFunction2
+
+function logMessage(message:any){
+  console.log(message+"<br>");
+  }
+ logMessage
+  // controla si se presiono un enter
+ /*let textarea:any=document.getElementById( "edit-acerca")
+  
+  textarea.addEventListener('keyup',(e) => {
+  logMessage(`Key "${e.key}"released [event:keyup]`) ;
+   if(e.key =="Enter"){
+         textarea.style.display="none"
+         document.getElementById("filel")
+             }
+       });
+*/
+  // carga el contenido de un archivo de texto
+  //y lo muestra en el parrafo
+  
+  function showFile(input:any) {
+       let file = input.files[0];-
+       alert(`File name: $ {file.name}`);// e.g my.png
+       alert(`Last modified:${file.lastModified}`) ;
+       showFile
+  let reader=new FileReader();
+       reader.readAsText(file);
+       reader.onload=function(progressEvent){
+       console.log(reader.result);
+       document.getElementById("text-acerca");
+  };
+  reader.onerror=function(){
+       console.log(reader.error);
+  };
+};
+
--- src/app/componentes/skills/skills.component.ts
+++ src/app/componentes/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/servicios/porfolio.service';
 
 @Component({
   selector: 'app-skills',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
+  skilist:any;
 
-  constructor() { }
+  constructor(private datosPorfolio:PorfolioService) { }
 
   ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.skilist=data.skills;
+    });
+    function ajusta():void {
+      let horizontales:any = document.querySelectorAll("#graficos .horizontal"); 
+      let elem:number= 0; 
+      while(elem < horizontales.length) {
+      horizontales[elem].style.transition = "background-size 2s ease-in-out 1s"; 
+      horizontales[elem].style.backgroundSize = horizontales[elem].innerHTML+" 100%"; 
+      elem++; 
+      }; 
+    }
+    onload = ajusta; 
+    
+   
   }
 
 }
